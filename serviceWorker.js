62d898fcc7248f9d2a298b770309e0885bcb0505
brainShakeCoctail <73761232+brainShakeCoctail@users.noreply.github.com>
2023-12-13 01:47:32 +0300

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,47 @@
+const CACHE = "V4";
+const URLS = [
+    "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/app.js"
+];
+
+let logFromSw = [];
+function myLog(e) {
+    logFromSw.push(e)
+}
+myLog("CACHE V4"),
+self.addEventListener("install", e=>{
+    myLog("Service Worker installing"),
+    self.skipWaiting(),
+    e.waitUntil(caches.open("V4").then(e=>e.addAll(URLS)))
+}
+),
+self.addEventListener("activate", e=>{
+    e.waitUntil(caches.keys().then(e=>Promise.all(e.map(e=>{
+        if ("V4" !== e)
+            return myLog(`Deleting cache ${e}`),
+            caches.delete(e)
+    }
+    ))).then(()=>(myLog("SW is activate and now ready to handle fetches!"),
+    self.clients.claim())))
+}
+),
+self.addEventListener("fetch", e=>{
+    if (e.request.url.endsWith("log.html")) {
+        const n = {
+            status: 200,
+            headers: {
+                "Content-type": "text/html"
+            }
+        };
+        let o = new Response(JSON.stringify(logFromSw),n);
+        logFromSw = [],
+        e.respondWith(o)
+    } else
+        e.respondWith(caches.match(e.request).then(n=>n ? (myLog(e.request.url + " from caches"),
+        n) : (myLog(e.request.url + " from server"),
+        fetch(e.request).then(n=>("error" !== n.type && caches.open("V4").then(o=>o.put(e.request, n)),
+        n.clone())))))
+}
+);
